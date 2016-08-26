79efc8a72eec5cff8cf8f1040f11fa5dccc4f206
masakij <masakij@users.noreply.github.com>
2016-08-26 11:07:13 +0900

    feat(remove): remove dangling images

--- docker-clean.sh
+++ docker-clean.sh
@@ -7,3 +7,11 @@ if [ -n "$DANGLING_CONTS" ]; then
 else
   echo "NO DANGLING CONTAINERS" $DANGLING_CONTS
 fi
+
+echo "-- REMOVE DANGLING IMAGES --"
+DANGLING_IMGS=`docker images -q -f dangling=true`
+if [ -n "$DANGLING_IMGS" ]; then
+  docker rmi $DANGLING_IMGS
+else
+  echo "NO DANGLING IMAGES" $DANGLING_IMGS
+fi
