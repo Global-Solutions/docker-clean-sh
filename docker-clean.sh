51778c25d79c5f94f081946f7c82bbf9ad88cf11
masakij <masakij@users.noreply.github.com>
2017-05-09 11:05:15 +0900

    feat(cleanup): add cleanup option
    * docker-clean.sh cleanupImage) cleanup images (remove all images not in use)
    * docker-clean.sh cleanupImageForce) cleanup containers $ images (remove all containers & images not in use)

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+case "$1" in
+  "cleanupImage")
+    CLEANUP_IMGS="true"
+  ;;
+  "cleanupImageForce")
+    CLEANUP_CONTS="true"
+    CLEANUP_IMGS="true"
+  ;;
+esac
+
 echo "-- REMOVE DANGLING CONTAINERS --"
 DANGLING_CONTS=`docker ps -q -f status=exited`
 if [ -n "$DANGLING_CONTS" ]; then
   docker rm -v $DANGLING_CONTS
 else
   echo "NO DANGLING CONTAINERS" $DANGLING_CONTS
+fi
+
+if [ -n "$CLEANUP_CONTS" ]; then
+  echo "-- CLEANUP CONTAINERS --"
+  docker rm $(docker ps -aq)
 fi
 
 echo "-- REMOVE DANGLING VOLUMES --"
@@ -22,4 +37,10 @@ if [ -n "$DANGLING_IMGS" ]; then
   docker rmi $DANGLING_IMGS
 else
   echo "NO DANGLING IMAGES" $DANGLING_IMGS
+fi
+
+if [ -n "$CLEANUP_IMGS" ]; then
+  echo "-- CLEANUP IMAGES --"
+  docker rmi $(docker images --format "{{.Repository}}:{{.Tag}}")
+  docker rmi $(docker images -q)
 fi
